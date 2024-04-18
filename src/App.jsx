d7b47a4ad5a008ba35a9fec8fa742d954c4936f7
Abdisa T. Bedada <152942085+abdi-gitter@users.noreply.github.com>
2024-04-17 23:08:28 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css"
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "heart", "horse"];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
+function App  () {
+    const [animals, setAnimals] = useState([]);
+    
+    const handleClick = () => {
+       setAnimals([...animals, getRandomAnimal()]);
+    };
+
+    const renderedAnimals = animals.map((animal, index) => {
+      return <AnimalShow type={animal} key={index} />
+    })
+
+  return (
+    <div className="app">
+      <button className="btn" onClick={handleClick}>Add Animal</button>
+      <div className="animals-list">{renderedAnimals}</div>
+    
+    </div>
+  );
+}
+
+export default App
