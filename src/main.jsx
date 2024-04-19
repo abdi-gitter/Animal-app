5b89f7c64964de696a4b84654579b5efdfe10540
Abdisa T. Bedada <152942085+abdi-gitter@users.noreply.github.com>
2024-04-18 22:33:15 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,6 @@ import ReactDom  from "react-dom/client";
 import App from "./App.jsx";
 
 
-const el = document.getElementById("root");
+const el = document.getElementById("root"); //The root is in index.html
 const root = ReactDom.createRoot(el);
 root.render(<App />); //App becomes the root
